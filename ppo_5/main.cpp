6f85116b67e86d4d40f2f2e5e4c9cedf223627b9
PawelZieba01 <zpawel.zieba@gmail.com>
2023-04-01 17:26:25 +0200

    Finish ppo_5

---
+++ ppo_5/main.cpp
@@ -0,0 +1,19 @@
+#include "Stepper.h"
+
+
+void Delay(int iTimeInMs){
+	int iCycle;
+	int iNumberOfCycles = 2400 * iTimeInMs;
+	
+	for (iCycle = 0; iCycle < iNumberOfCycles; iCycle++) {}
+}
+
+Stepper MyStepper;
+
+int main(void)
+{
+	while(1){
+		Delay(100);
+		MyStepper.StepLeft();
+	}
+}
